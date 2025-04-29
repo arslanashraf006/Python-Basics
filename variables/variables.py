bd2731907c24f499a59556ba1643d8eb6660568b
arslanashraf006 <arslanashraf7864@gmail.com>
2025-04-29 16:28:41 +0500

    Added : Variables -> python variables

---
+++ variables/variables.py
@@ -0,0 +1,36 @@
+# monthly expenses
+
+rent = 1220
+gas = 202.5
+grocerires = 305.6
+
+print(rent)
+
+total = rent + gas + grocerires
+
+print(total)
+
+rent = 1400
+
+item1 = "rent"
+item2 = "gas"
+item3 = "groceries"
+
+print("Expense items:", item1, item2, item3)
+
+# exercise
+# 1
+birthYear = 2005
+currentYear = 2025
+
+age =  currentYear - birthYear
+
+print("age is : ", age)
+
+# 2
+
+firstName = "Arslan"
+middleName = " "
+lastName = "Ashraf"
+
+print("name is : ", firstName, middleName, lastName)
