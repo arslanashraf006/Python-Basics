fed9b80a2480b750f0f6761004ab4fd92245b4c0
arslanashraf006 <arslanashraf7864@gmail.com>
2025-05-07 10:37:00 +0500

    Added : Numbers -> python numbers practice

---
+++ numbers/numbers.py
@@ -0,0 +1,68 @@
+print(12 + 34)
+print(3-4)
+print(3/4)
+print(4 * 5)
+print(11 % 2)
+
+#power
+print(3**2)
+print(2**4)
+
+# total distance
+nyc_bal = 188
+bal_pitt = 247
+total_distance = nyc_bal + bal_pitt
+
+print(total_distance)
+
+#total time spent in travel
+mph = 65
+time = total_distance / mph
+
+print(time)
+
+#round numbers to max decimal ponts
+print(round(time, 2))
+print(round(time, 3))
+
+print(10+2*3)
+print((10+2)*3)
+print(6-5.7)
+print(round(6-5.7,2))
+
+# Exercise
+
+#1
+field_length = 72
+field_width = 48.8
+
+area = field_length * field_width
+print("Total area of the football field is:", area, "square meters")
+
+#2
+
+total_chips = 9
+one_pack_cost = 1.49
+amount_given = 20
+
+return_money = amount_given - (total_chips * one_pack_cost)
+
+print("Dollar given back to you : ", return_money)
+
+
+#3
+
+side_length = 5.5  # in feet
+cost_per_sqft = 500  # in Rs
+
+area = side_length ** 2
+total_cost = area * cost_per_sqft
+
+print("Total cost to replace all tiles is:", total_cost, "Rs")
+
+#4
+
+number = 17
+binary = bin(number)
+print("Binary representation of 17 is:", binary)
+
